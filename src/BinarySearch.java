1c6b91fa8c06d968189f70d15f0a9105157f96fe
hp <pajaykuumar@gmail.com>
2024-05-27 07:32:22 +0530

    ajaypadedam789@gmail.com

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class BinarySearch {
+public class BinarySearch {
+    static int binarySearch(int[] arr,int x){
+        int low=0;
+        int high=arr.length-1;
+        while(low<high){
+            int mid=(low+high)/2;
+            if (arr[mid]==x){
+                return mid;
+            }else if (arr[mid]>x){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={7,10,12,16,22,24,32,40};
+        System.out.println("Element found at index "+binarySearch(arr,24));
+    }
 }
